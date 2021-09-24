41cb888fb77f198254542b437eade4cfc63fd863
Sajan Kumar <bhagat.sajan0073@gmail.com>
2021-09-24 18:07:18 +0530

    Custom Designed Website
    Custom Designed Website

--- app.py
+++ app.py
@@ -6,3 +6,10 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template('index.html')
+
+@app.route("/latest")
+def profile():
+    return render_template("index_latest.html")
+
+if __name__ == "__main__":
+    app.run(debug=True,port=8080)
